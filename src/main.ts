2b5c96417f4703a146610dccb18f2bf925a70627
diegofreitas50 <diegofreitas50@hotmail.com>
2022-06-13 17:30:57 -0300

    proecss.env.PORT

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -29,6 +30,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3333);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
